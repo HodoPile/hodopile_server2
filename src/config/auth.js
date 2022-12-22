ed48538161a95931967ed9a48d6e94ce3fef6f30
antoni <antoni.dev0@gmail.com>
2022-12-21 20:14:24 -0800

    test production

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,11 +1,12 @@
 const { auth } = require('express-openid-connect');
 
+let SERVER_URL = ( process.env.NODE_ENV === "production" )? process.env.HEROKU_SERVER_URL : process.env.LOCAL_SERVER_URL;
 module.exports  = {
     config: {
         authRequired: false,
         auth0Logout: true,
         secret: process.env.AUTH0_SECRET,
-        baseURL: process.env.LOCAL_SERVER_URL,
+        baseURL: SERVER_URL,
         clientID: process.env.AUTH0_CLIENTID,
         issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
     }
